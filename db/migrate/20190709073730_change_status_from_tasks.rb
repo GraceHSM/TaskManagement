eca52930ec7d9b781084558623ae7bab05b7eccb
Grace <syen741205@gmail.com>
2019-07-09 16:55:59 +0800

    修改migration，確定能安全回到上一步的狀態

--- db/migrate/20190709073730_change_status_from_tasks.rb
+++ db/migrate/20190709073730_change_status_from_tasks.rb
@@ -1,5 +1,10 @@
 class ChangeStatusFromTasks < ActiveRecord::Migration[5.2]
   def change
-    change_column :tasks, :status, 'integer USING CAST(status AS integer)'
+    reversible do |dir|
+      change_table :tasks do |t|
+        dir.up   { t.change :status, 'integer USING CAST(status AS integer)' }
+        dir.down { t.change :status, :string }
+      end
+    end
   end
 end
