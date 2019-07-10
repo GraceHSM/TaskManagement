873399eb2d63c13531b6788589030ec2b8b8ed5f
Grace <syen741205@gmail.com>
2019-07-10 10:46:52 +0800

    Setting enum of status and priority on Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  enum status: [ :pending, :processing, :completed ]
+  enum priority: [ :primary, :secondly, :thirdly, :common ]
   belongs_to :user
   has_many :task_tags
   has_many :tags, through: :task_tags
