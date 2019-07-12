f97ba202a347d810501eaf1644f876e3348113b3
Grace <syen741205@gmail.com>
2019-07-12 16:43:13 +0800

    feature rspec of delete

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -8,43 +8,40 @@ RSpec.describe Task, :type => :feature do
 
   it "Display tasks" do
     3.times{
-      Task.create(title: title,content: content)
+      Task.create(title: title, content: content)
     }
     expect(Task.count).to be 3
-
     visit tasks_path
     expect(page).to have_text(Task.third.title)
-
   end
 
   it "Create a new task" do
     visit new_task_path
-    fill_in "task_title", :with => title
-    fill_in "task_content", :with => content
-    choose('pending')
-    choose('secondly')
-    click_button "確定"
+    create_task(title, content)
     expect(page).to have_content(title)
     expect(page).to have_content(content)
   end
 
   it "Edit a task" do
-    task = Task.create(title: title,content: content)
+    task = Task.create(title: title, content: content)
     visit edit_task_path(task.id)
-    fill_in "task_title", :with => new_title
-    fill_in "task_content", :with => new_content
-    choose('pending')
-    choose('secondly')
-    click_button "確定"
+    create_task(new_title, new_content)
     expect(page).to have_content(new_title)
     expect(page).to have_content(new_content)
   end
 
-  # it "Destroy a task" do
-    # task = Task.create(title: title,content: content)
-    # expect{ delete :Delete, :id => task.id }.to change(Task, :count).by(-1)
+  it "Destroy a task" do
+    task = Task.create(title: title, content: content)
+    visit tasks_path
+    expect{ click_on "Delete"}.to change{ Task.count }.by(-1)
+  end
 
-    # expect(find('Delete')).to change(Task.count).by(-1)
-    # find("a[href*='#{task_path(task.id)}']").click
-  # end
+  private
+  def create_task(title, content)
+    fill_in "task_title", :with => title
+    fill_in "task_content", :with => content
+    choose('pending')
+    choose('secondly')
+    click_button "確定"
+  end
 end
