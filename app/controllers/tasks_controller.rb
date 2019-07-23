bf288f3dd3f39d0e7d3e5b633e2eacda7f6c8493
Grace <syen741205@gmail.com>
2019-07-23 17:28:57 +0800

    將 View / Controller / Model 中的語言部份共用化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to root_path, notice: '新增成功'
+      redirect_to root_path, notice: t('create_success')
     else
       render :new
     end
@@ -24,7 +24,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to root_path, notice: '修改成功'
+      redirect_to root_path, notice: t('edit_success')
     else
       render :edit
     end
@@ -32,7 +32,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy if @task
-    redirect_to root_path, notice: '刪除成功'
+    redirect_to root_path, notice: t('delete_success')
   end
 
   private
