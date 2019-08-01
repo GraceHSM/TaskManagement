25f3f191b88ffb2bd9e5d1faaf688ed78e625539
Grace <syen741205@gmail.com>
2019-08-01 16:20:29 +0800

    model spec : search feature

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  before :each do
+    create(:task, title: 'abc123', status: 0)
+    create(:task, title: 'c12xy', status: 2)
+    create(:task, title: 'xyz', status: 2)
+  end
+  describe 'Search feature' do
+    it 'title' do
+      task = Task.ransack(title_cont:'c12').result
+      expect(task.count).to eq 2
+    end
+
+    it 'status' do
+      task = Task.ransack(status_eq:'2').result
+      expect(task.count).to eq 2
+    end
+
+    it 'title and status' do
+      task = Task.ransack(title_cont:'c12', status_eq:'2').result
+      expect(task.count).to eq 1
+    end
+  end
 end
