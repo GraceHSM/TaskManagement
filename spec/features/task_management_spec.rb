9dad9745cadbcda3f8e4b91c9ea4986a95c2d728
Grace <syen741205@gmail.com>
2019-08-03 11:09:43 +0800

    修正rspec

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Task, :type => :feature do
 
     it 'Create' do
       fill_in_new_task
-      create(:task, user_id: user.id)
+      task
       click_button I18n.t('submit')
       check_page(I18n.t('create_success'))
     end
