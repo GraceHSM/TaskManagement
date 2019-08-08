594f7171379c4aa71b16f63f5a60b5634f79e926
Grace <syen741205@gmail.com>
2019-08-09 02:18:28 +0800

    task spec on travis

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   let(:user){ create(:user) }
+  let(:title) { Faker::Lorem.sentence }
+  let(:content) { Faker::Lorem.paragraph }
+  let(:start_at) { DateTime.now - 1 }
+  let(:deadline_at) { DateTime.now + 1 }
+  let(:priority) { ['primary', 'secondly', 'common'].sample }
+
   describe 'Search feature' do
     before :each do
-      user
-      create(:task, user_id: user.id, title: 'abc123', status: 'pending')
-      create(:task, user_id: user.id, title: 'c12xy', status: 'completed')
-      create(:task, user_id: user.id, title: 'xyz', status: 'completed')
+      create_task('abc123', 'pending')
+      create_task('c12xy', 'completed')
+      create_task('xyz', 'completed')
     end
     it 'title' do
       task = Task.ransack(title_cont: 'c12').result
@@ -23,5 +28,10 @@ RSpec.describe Task, type: :model do
       task = Task.ransack(title_cont: 'xyz', status_eq: '2').result
       expect(task.count).to eq 1
     end
+  end
+
+  private
+  def create_task(title, status)
+    user.tasks.create(title: title, content: content, start_at: start_at, deadline_at: deadline_at, priority: priority, status: status)
   end
 end
