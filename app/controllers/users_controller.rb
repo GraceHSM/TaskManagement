1e62c0128bdd5e47ac02b33df938773cfaea9461
Grace <syen741205@gmail.com>
2019-08-08 14:05:47 +0800

    complete password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password,  :password_confirmatioin, :role)
+    params.require(:user).permit(:username, :email, :password,  :password_confirmation, :role)
   end
 
   def user_find
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :email, :username, :password, presence: true
   validates :email, uniqueness: true
-  validates :password, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
   validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
 
   enum role: [:member, :admin]
 
