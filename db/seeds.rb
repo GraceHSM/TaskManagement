6b8e59cf2dfe8e9da2a74e1bd2f2a4271eb8f753
Grace <syen741205@gmail.com>
2019-08-08 14:49:45 +0800

    Create data with password confirmation by seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create(username: 'admin', email: 'admin@gmail.com', password: '111111', role: 1)
+User.find_or_create_by(username: 'admin', email: 'admin@gmail.com', password: '111111', role: 1)
 
 10.times{
-  u = FactoryBot.create(:user)
+  u = FactoryBot.create(:user, password_confirmation: '111111')
   task = rand 0..10
   task.times{
     FactoryBot.create(:task, user_id: u.id)
