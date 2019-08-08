337c9648158015e769baa5a600334fc18fafe67b
Grace <syen741205@gmail.com>
2019-08-09 01:46:21 +0800

    replace before action

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   let(:user){ create(:user) }
-  before :each do
-    user
-    create(:task, user_id: user.id, title: 'abc123', status: 'pending')
-    create(:task, user_id: user.id, title: 'c12xy', status: 'completed')
-    create(:task, user_id: user.id, title: 'xyz', status: 'completed')
-  end
   describe 'Search feature' do
+    before :each do
+      user
+      create(:task, user_id: user.id, title: 'abc123', status: 'pending')
+      create(:task, user_id: user.id, title: 'c12xy', status: 'completed')
+      create(:task, user_id: user.id, title: 'xyz', status: 'completed')
+    end
     it 'title' do
       task = Task.ransack(title_cont: 'c12').result
       expect(task.count).to eq 2
