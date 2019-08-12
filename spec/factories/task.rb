5fdfe4754fc391636a9336dbc83265d974f19460
Grace <syen741205@gmail.com>
2019-08-12 20:54:10 +0800

    edit factory task data

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     title { Faker::Lorem.sentence }
     content { Faker::Lorem.paragraph }
     start_at { DateTime.now - rand(2..4) }
-    deadline_at { DateTime.now + 1 }
+    deadline_at { DateTime.now + rand(2..4) }
+    created_at { DateTime.now - rand(4..10) }
     priority { rand 0..2 }
     status { rand 0..2 }
   end
