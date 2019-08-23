c60bc4e01768874dc8abdf125939c06c7998b083
Grace <syen741205@gmail.com>
2019-08-23 22:23:02 +0800

    Add dragEnter and dragLeave

--- app/javascript/modules/sort/dragAndDrop.js
+++ app/javascript/modules/sort/dragAndDrop.js
@@ -1,9 +1,10 @@
 let items = document.querySelectorAll('.list')
 items.forEach( item => {
-  item.addEventListener('dragstart', dragStart)
-  item.addEventListener('drop', dropped)
-  item.addEventListener('dragenter', cancelDefault)
-  item.addEventListener('dragover', cancelDefault)
+  item.addEventListener('dragstart', dragStart, false)
+  item.addEventListener('drop', dropped, false)
+  item.addEventListener('dragenter', dragEnter, false)
+  item.addEventListener('dragleave', dragLeave, false)
+  item.addEventListener('dragover', cancelDefault, false)
 })
 
 function dragStart (ev) {
@@ -24,10 +25,16 @@ function dropped (ev){
 
   //move drag element before drop element
   target.insertAdjacentElement('beforebegin', content)
+  target.style.borderTop = 'none'
 }
 
+function dragEnter(ev){
+  ev.currentTarget.style.borderTop = '5px solid #007bff'
+}
+function dragLeave(ev){
+  ev.currentTarget.style.borderTop = 'none'
+}
 function cancelDefault(ev){
   ev.preventDefault()
   ev.stopPropagation()
-  return false
 }
